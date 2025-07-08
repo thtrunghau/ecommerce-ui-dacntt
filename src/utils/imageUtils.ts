5dcd4bea05f6949c9ca52f5de2379e36977a9f7e
Trung Hau <52100025@student.tdtu.edu.vn>
2025-07-09 00:22:39 +0700

    re-update getProductImageUrl

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -1,6 +1,6 @@
 // Chuẩn hóa lấy link ảnh sản phẩm từ S3 bucket qua biến môi trường
 export function getProductImageUrl(imagePath?: string) {
   if (!imagePath) return "/images/products/placeholder.png";
-  // return `https://${import.meta.env.VITE_IMAGE_URL_BUCKET_NAME}.s3.${import.meta.env.VITE_IMAGE_URL_AREA}.amazonaws.com/${imagePath}`;
-  return `https://finalterm-spring-bucket.s3.ap-southeast-1.amazonaws.com/${imagePath}`;
+  return `https://${import.meta.env.VITE_IMAGE_URL_BUCKET_NAME}.s3.${import.meta.env.VITE_IMAGE_URL_AREA}.amazonaws.com/${imagePath}`;
+  // return `https://finalterm-spring-bucket.s3.ap-southeast-1.amazonaws.com/${imagePath}`;
 }
